52c10aa5605a71560421387e491daa7a58a5922a
HimadriSri <121866125+HimadriSri@users.noreply.github.com>
2024-10-02 02:57:48 +0000

    Datatypes

---
+++ test/test.js
@@ -0,0 +1,21 @@
+// Declaring variables and constant-
+// 	1. Const acctID = 12345
+// 	2. Let acctName = 'Himadri' ---Preferred
+// 	3. acctDept = "Deloittte"                     Not good as per coding standards, VAR doesn't have definite block scope and functional scope                 
+// 	4. Var acctDept = "Deloitte"
+// Data types 
+// 	1. "Use strict" ---this treats whole code as per newer JS coding standard
+// 	2. Alert(3 + 3) ---this will give result in pop up screen as 6 in a browser but if writing code under NodeJS, this is not valid, there is different way to write in NodeJS.
+// 	Datatype	Comments
+// 	Number	Range upto 2 to power 53
+// 	String	""
+// 	Bigint	Values> Number datatype
+// 	Boolean	T/F
+// 	Null	Standalone value--can assign to any variable which means assigning empty value
+// 	undefined	Value not assigned
+// 	Symbol	Unique
+// 	object	
+	
+// 	3. Console.log(typeof va) --this returns data type of that var
+// 	4. If you type - console.log(typeof null) ---it returns object----this means null is an object
+// Console.log(typeof undefined) --gives undefined
