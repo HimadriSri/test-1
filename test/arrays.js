4ce79214e8163a115c663b4cba60c6435480c6da
HimadriSri <121866125+HimadriSri@users.noreply.github.com>
2024-10-04 06:51:41 +0000

    part1

---
+++ test/arrays.js
@@ -0,0 +1,8 @@
+// const arr1 = new Array(0,1,2,3,4,5)
+// console.log(arr1);
+
+
+// arr1.push(3);---Pushes new value in array
+// Arr1.pop()----Pop --deletes last value
+
+// Arr1.unshift(9)----Unshift --adds value at 0th index, should not be used much as it will arr1.shift()----have to move all values to one place next--increases load on computer
